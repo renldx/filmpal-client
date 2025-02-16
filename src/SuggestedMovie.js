d9d7cd4f4812b3776dda44194f0873aedfc55a32
renldx <renldx@gmail.com>
2025-02-16 16:48:48 -0500

    Implement movie detail API call

--- src/SuggestedMovie.js
+++ src/SuggestedMovie.js
@@ -5,52 +5,32 @@ const SuggestedMovie = ({ movie, releaseYear, toggleModal }) => {
     const [movieData, setMovieData] = useState(null);
 
     useEffect(() => {
-        let ignore = false;
+        fetch(`/api/details/movie?code=${movie.code}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovieData(data);
+            });
+    }, [movie.code]);
 
-        setMovieData(null);
-
-        try {
-            fetch(`/api/details/movie?imdbId=${movie.imdbId}`)
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error(
-                            `Failed to fetch movie details for ${movie.title} (${movie.imdbId})`,
-                        );
-                    } else {
-                        response.json();
-                    }
-                })
-                .then((data) => {
-                    if (!ignore) {
-                        setMovieData(data);
-                    }
-                });
-        } catch (error) {
-            return <></>;
-        }
-        return () => {
-            ignore = true;
-        };
-    }, [movie.imdbId, movie.title]);
-
-    if (movieData) {
-        return (
-            <Card
-                onClick={(event) => {
-                    toggleModal(event, movie);
-                }}
-                className="suggested-movie">
-                <img alt="Sample" src="https://picsum.photos/300/200" />
-                <CardBody>
-                    <CardTitle tag="h4">{movie.title}</CardTitle>
-                    <CardSubtitle tag="h5" className="mb-2 text-muted">
-                        {releaseYear(movie.release)}
-                    </CardSubtitle>
-                    <CardText></CardText>
-                </CardBody>
-            </Card>
-        );
-    }
+    return (
+        <Card
+            onClick={(event) => {
+                toggleModal(event, movie);
+            }}
+            className="suggested-movie">
+            <img
+                className="poster"
+                alt="Movie Poster"
+                src={movieData?.poster}></img>
+            <CardBody>
+                <CardTitle tag="h4">{movie.title}</CardTitle>
+                <CardSubtitle tag="h5" className="mb-2 text-muted">
+                    {releaseYear(movie.release)}
+                </CardSubtitle>
+                <CardText></CardText>
+            </CardBody>
+        </Card>
+    );
 };
 
 export default SuggestedMovie;
